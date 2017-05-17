473507426a36dc73d856dfc7073820fa1dfd8d6d
Filipe Bastos <filipe@filipebastos.com>
2017-05-16 21:14:56 -0300

    Add filtro

--- taskmanager/taskmanager-gui/src/main/java/com/taskmanager/controller/PanelView.java
+++ taskmanager/taskmanager-gui/src/main/java/com/taskmanager/controller/PanelView.java
@@ -25,41 +25,39 @@ public class PanelView implements Serializable {
 	private ITaskService service = new TaskService();
 
 	private Session session = Session.getInstance();
-	
+
 	private Task task;
-	
+
 	private String description;
-	
+
 	public PanelView() {
 		reset();
 	}
 
 	public Collection<Task> listAll() {
-
-		teste();
-
+		init();
 		List<Task> lista = new ArrayList<>(service.listBySituation(true));
 		Collections.reverse(lista);
 		return lista;
 	}
-	
+
 	public void taskSelect(Task task) {
-		this.task = task;		
+		this.task = task;
 	}
 
 	public void taskClose() {
 		this.task.setOpen(false);
 		taskSave();
 	}
-	
+
 	public void taskSave() {
-		
+
 		try {
-			
+
 			service.update(this.task);
 			reset();
 			listAll();
-			
+
 		} catch (TaskManagerException e) {
 			e.printStackTrace();
 		}
@@ -68,72 +66,74 @@ public class PanelView implements Serializable {
 	public boolean assignedToLogged() {
 		return session.getUserLogged().equals(this.task.getOwner());
 	}
-	
+
 	public boolean withoutAttribution() {
 		return this.task.getOwner() == null;
 	}
-	
+
 	public boolean assignedToAnother() {
 		return !withoutAttribution() && !assignedToLogged();
 	}
-	
-	public void leaveTask() {		
+
+	public void leaveTask() {
 		this.task.setOwner(null);
 		taskSave();
 	}
-	
-	public void getTaskWithoutAttribution() {		
+
+	public void getTaskWithoutAttribution() {
 		this.task.setOwner(session.getUserLogged());
 		taskSave();
 	}
 
 	public void create() {
 		try {
-			
+
 			Task newTask = new Task();
 			newTask.setDescription(description);
 			service.insert(newTask);
 			reset();
-			
+
 		} catch (TaskManagerException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	public void closeWindowTaskDetail() {
+		reset();
+	}
+
 	private void reset() {
 		this.task = new Task();
 		this.description = "";
 	}
 
-	// TODO Remover após teste
-	private void teste() {
+	private void init() {
 
 		if (service.listAll().isEmpty()) {
-
 			try {
-				
+
 				new TaskService().insert(new Task(Session.getInstance().getUserLogged(), "Ajustar processo de deploy na nuvem"));
-				new TaskService().insert(new Task(null, "XXXXXXXXXX XXXXXXXXXXX asndja skdjh kasjdh kasjh dkjh as"));
-				new TaskService().insert(new Task(new User("Hugo", "hugo", "000"), "WWW aksd kasj djagsh dgajsgjas"));
-				
+				new TaskService().insert(new Task(null, "Revisar infraestrutura da sala 4"));
+				new TaskService().insert(new Task(new User("Hugo", "hugo", "000"), "Cria testes de aceitação para App X"));
+
 			} catch (TaskManagerException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
 	public Task getTask() {
 		return task;
 	}
-	
+
 	public void setTask(Task task) {
 		this.task = task;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
-	
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
